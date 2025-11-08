8bfccf6da4332b0cdbc49224e67d39ffeb6cf7c3
ywhnaht <hothanhhuy24062004@gmail.com>
2025-11-08 07:55:32 +0700

    add model, connect to database

---
+++ backend/src/main/java/org/example/backend/model/entity/Amenity.java
@@ -0,0 +1,24 @@
+package org.example.backend.model.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+import lombok.experimental.FieldDefaults;
+
+@Entity
+@Table(name = "amenities")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@FieldDefaults(level = AccessLevel.PRIVATE)
+public class Amenity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    Long id;
+
+    @Column(unique = true, nullable = false)
+    String name;
+
+    @Column(name = "icon_class")
+    String iconClass;
+}
---
+++ backend/src/main/java/org/example/backend/model/entity/RentalImage.java
@@ -0,0 +1,28 @@
+package org.example.backend.model.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+import lombok.experimental.FieldDefaults;
+
+@Entity
+@Table(name = "rental_images")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@FieldDefaults(level = AccessLevel.PRIVATE)
+public class RentalImage {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    Long id;
+
+    @Column(name = "image_url", nullable = false)
+    String imageUrl;
+
+    @Column(name = "alt_text")
+    String altText;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+            @JoinColumn(name = "rental_id", nullable = false)
+    Rental rental;
+}
