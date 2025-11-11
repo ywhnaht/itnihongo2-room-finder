2edebf52ea7e411dc1da7833db41b9dec977afd2
ywhnaht <hothanhhuy24062004@gmail.com>
2025-11-11 22:57:56 +0700

    add api documment

--- backend/src/main/java/org/example/backend/BackendApplication.java
+++ backend/src/main/java/org/example/backend/BackendApplication.java
@@ -2,8 +2,10 @@ package org.example.backend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 @SpringBootApplication
+@EnableWebMvc
 public class BackendApplication {
 
     public static void main(String[] args) {
